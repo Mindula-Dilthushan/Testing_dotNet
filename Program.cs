60f8eee047f2ac988cc0d11e106032f67197b5d1
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2021-08-14 06:42:04 +0530

    print hello

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mindula!");
+        }
+    }
+}
